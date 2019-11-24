a6d9fe7576c5c929a282168c42f75daf58856a8a
William Blacknall <williamblacknall@gmail.com>
2019-11-23 23:59:17 -0500

    Used map function successfully to log previous responses

--- src/Components/EightBallCard.js
+++ src/Components/EightBallCard.js
@@ -1,18 +1,27 @@
 import React from "react";
 
+
 export default class EightBallCard extends React.Component {
 	render() {
+		const responses = this.props.response.map(response => {
+			return <p>{response}</p>;
+		});
 		return (
 			<div className="section">
 				<div className="content">
 					<p className="has-text-centered">{this.props.question}</p>
 					<p className="has-text-centered">{this.props.answer}</p>
 				</div>
-                <div className="content">
-                    <p className="has-text-centered">
-                        
-                    </p>
-                </div>
+				<div className="content">
+					<div className="tile is-ancestor box">
+						<div className="tile is-parent">
+							<div className="tile is-child">
+								<p className="title has-text-centered">Response Log</p>
+								{responses}
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		);
 	}
